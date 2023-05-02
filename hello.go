aa876c5c902c70903529c164ca03406c1daaae3c
danieljhark <60290668+danieljhark@users.noreply.github.com>
2023-05-02 10:49:14 +0100

    array method

--- hello.go
+++ hello.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hello world")
+	helloArray := [2]string{}
+	helloArray[0]="hello"
+	helloArray[1]="BJSS"
+	for _, s := range helloArray{
+		fmt.Print(s, " ")
+	}
 }
